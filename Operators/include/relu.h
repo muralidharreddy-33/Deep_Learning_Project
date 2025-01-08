08cb25349a8689e26a03d7a238a8f3f4ee2119ca
Muralidhar Redy <patimidimurali@gmail.com>
2025-01-08 11:37:46 +0530

    modify_relu.h

--- Operators/include/relu.h
+++ Operators/include/relu.h
@@ -2,20 +2,10 @@
 #define RELU_H
 
 #include <vector>
-#include <string>
-#include <fstream>
-#include <iostream>
 
-class ReLULayer {
+class ReLU {
 public:
-    ReLULayer(const std::string& input_path);
-    void execute(const std::string& output_path);
-
-private:
-    std::vector<float> input;
-
-    void load_bin_data(const std::string& path, std::vector<float>& data);
-    void save_bin_data(const std::string& path, const std::vector<float>& data);
+    std::vector<float> forward(const std::vector<float>& input);
 };
 
 #endif // RELU_H
